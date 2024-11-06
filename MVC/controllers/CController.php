e15cfe98adf6ef3b7083414c39848f60657bce54
Zahharius2 <148521669+Zahharius2@users.noreply.github.com>
2024-11-06 14:42:05 +0200

    Add files via upload

---
+++ MVC/controllers/CController.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once ('./models/CModel.php');
+require_once ('./views/CView.php');
+
+class CController {
+    public $model;
+    public $view;
+
+    public function SetMainPage () {
+        $model = new CModel();
+        $resultModel = $model->GetData();
+
+        return $resultModel;
+    }
+
+    
+}
